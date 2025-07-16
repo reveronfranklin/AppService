34e2960d16f9ba3964c3a121ebd7cd978b2fbc7f
reveronfranklin <reveronfranklin@gmail.com>
2025-07-16 09:35:48 -0400

    Reclacular Precio al integrar

--- Automata/Program.cs
+++ Automata/Program.cs
@@ -172,7 +172,9 @@ internal class Program
         serviceCollection.AddTransient<IAppRecipesByAppDetailQuotesService, AppRecipesByAppDetailQuotesService>();
         serviceCollection.AddTransient<IAppDetailQuotesService, AppDetailQuotesService>();
         serviceCollection.AddTransient<IAppStatusQuoteService, AppStatusQuoteService>();
-        serviceCollection.AddTransient<ITPaTasaReferencialServices, ITPaTasaReferencialServices>();
+        serviceCollection.AddTransient<ITPaTasaReferencialServices, TPaTasaReferencialServices>();
+        
+        
         serviceCollection.AddTransient<IAppTemplateConversionUnitService, AppTemplateConversionUnitService>();
         serviceCollection.AddTransient<IAppDetailQuotesConversionUnitService, AppDetailQuotesConversionUnitService>();
 
